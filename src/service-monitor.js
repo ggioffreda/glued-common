c5cfc0c328a88a99e30aced103ee4b6e30b579d9
Giovanni Gioffreda <giovanni.gioffreda@gmail.com>
2016-10-03 19:30:23 +1300

    Broadcast an initialisation message on service initialisation.

--- src/service-monitor.js
+++ src/service-monitor.js
@@ -27,6 +27,7 @@ function ServiceMonitor (service, messageBusChannel) {
       subscribe('state.monitor')
       subscribe(['state', name, 'monitor'].join('.'))
     }
+    messageBusChannel.publish(['monitor', 'init', name, id], Date.now())
   }
 
   this.setPackageInformation = function (info) {
