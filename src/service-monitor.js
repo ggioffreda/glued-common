bcc943abe97c070d34b1a9b6069379d4a67336e5
Giovanni Gioffreda <giovanni.gioffreda@gmail.com>
2016-10-24 09:32:33 +1300

    Add subscriber for OS info retrieval.

--- src/service-monitor.js
+++ src/service-monitor.js
@@ -15,6 +15,8 @@ function ServiceMonitor (service, messageBusChannel) {
         self.pong(content)
       } else if (key.substring(0, 5) === 'state') {
         self.state()
+      } else if (key.substring(0, 4) === 'info') {
+        self.info()
       }
       cb()
     }
@@ -25,12 +27,21 @@ function ServiceMonitor (service, messageBusChannel) {
     subscribe('ping.monitor')
     subscribe(['ping', name, 'monitor'].join('.'))
     subscribe(['ping', name, id, 'monitor'].join('.'))
+    subscribe('info.monitor')
+    subscribe(['info', name, 'monitor'].join('.'))
+    subscribe(['info', name, id, 'monitor'].join('.'))
     if (sm.serviceDefines(service, 'getState')) {
       subscribe('state.monitor')
       subscribe(['state', name, 'monitor'].join('.'))
       subscribe(['state', name, id, 'monitor'].join('.'))
     }
     messageBusChannel.publish(['monitor', 'init', name, id].join('.'), Date.now())
+  }
+
+  function checkName() {
+    if (name === null) {
+      throw new Error('An unnamed service cannot pong')
+    }
   }
 
   this.setPackageInformation = function (info) {
@@ -42,16 +53,12 @@ function ServiceMonitor (service, messageBusChannel) {
   }
 
   this.pong = function (content) {
-    if (name === null) {
-      throw new Error('An unnamed service cannot pong')
-    }
+    checkName()
     messageBusChannel.publish(['monitor', 'pong', name, id].join('.'), content)
   }
 
   this.state = function () {
-    if (name === null) {
-      throw new Error('An unnamed service cannot pong')
-    }
+    checkName()
     var state = sm.serviceDefines(service, 'getState') ? service['getState']() : {}
     const info = self.getPackageInformation()
     messageBusChannel.publish(['monitor', 'state', name, id].join('.'), {
@@ -60,6 +67,28 @@ function ServiceMonitor (service, messageBusChannel) {
       version: info && info.version ? info.version : 'unknown',
       hostname: os.hostname()
     })
+  }
+
+  this.info = function () {
+    checkName()
+    messageBusChannel.publish(['monitor', 'info', name, id].join('.'), {
+      os: {
+        arch: os.arch(),
+        cpus: os.cpus(),
+        endianness: os.endianness(),
+        freemem: os.freemem(),
+        homedir: os.homedir(),
+        hostname: os.hostname(),
+        loadavg: os.loadavg(),
+        network_interfaces: os.networkInterfaces(),
+        platform: os.platform(),
+        release: os.release(),
+        tmpdir: os.tmpdir(),
+        totalmem: os.totalmem(),
+        type: os.type(),
+        uptime: os.uptime()
+      }
+    });
   }
 }
 
